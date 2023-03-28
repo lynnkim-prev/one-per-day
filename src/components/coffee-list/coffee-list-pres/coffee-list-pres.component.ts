0471ac1a1bfe62c7435685b649098c71730a7070
Lynn Kim <lynn.kim@ibm.com>
2023-03-28 01:24:11 -0300

    feat(lw-03251): created UI with mock data

--- src/components/coffee-list/coffee-list-pres/coffee-list-pres.component.ts
+++ src/components/coffee-list/coffee-list-pres/coffee-list-pres.component.ts
@@ -1,16 +1,43 @@
 import {
   Component,
-  Input
+  Input,
+  OnChanges,
+  SimpleChanges
 } from '@angular/core';
+import { CoffeeItem } from "../../../models/coffee.model";
 
 @Component({
   selector: 'app-coffee-list-pres',
   templateUrl: './coffee-list-pres.component.html',
   styleUrls: ['./coffee-list-pres.component.scss']
 })
-export class CoffeeListPresComponent {
+export class CoffeeListPresComponent implements OnChanges {
 
+  /* Array of coffee items */
   @Input()
-  public coffees: any = '';
+  public coffees!: CoffeeItem[];
 
+  /* Number of keys in coffee item */
+  public numberOfCoffeeKeys: number = 0;
+
+  /* Detect input changes */
+  public ngOnChanges(changes: SimpleChanges) {
+    if (changes.coffees) {
+      this.numberOfCoffeeKeys = Object.keys(this.coffees[0]).length;
+    }
+  }
+
+  /* Initial page number */
+  public page: number = 1;
+
+  /* Count of items */
+  public count: number = 0;
+
+  /* Number of items to display */
+  public tableSize: number = 10;
+
+  /* Update page number on table number click */
+  public onTableDataChange(event: any) {
+    this.page = event;
+  }
 }
---
+++ src/models/coffee.model.ts
@@ -0,0 +1,12 @@
+/**
+ * Interface for coffee item
+ */
+export interface CoffeeItem {
+  id: number;
+  uid: string;
+  blend_name: string;
+  origin: string;
+  variety: string;
+  notes: string;
+  intensifier: string;
+}
